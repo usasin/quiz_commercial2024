045bb394338226229869c81bf276a83f71d49fc4
usain <ibabhh@gmail.com>
2025-01-03 10:53:34 +0100

    Mise à jour des fichiers iOS et dépendances Flutter

--- ios/Flutter/podhelper.rb
+++ ios/Flutter/podhelper.rb
@@ -1,50 +1,22 @@
-# podhelper.rb
-
-require 'json'
-
 def parse_KV_file(file, separator='=')
-    file_abs_path = File.expand_path(file)
-    if !File.exist?(file_abs_path)
-        return {}
-    end
-    File.foreach(file_abs_path).with_object({}) do |line, map|
-        line = line.strip
-        if line.start_with?('#')
-            next
-        end
-        key, value = line.split(separator, 2)
-        if key && value
-            map[key.strip] = value.strip
-        end
-    end
+  file_abs_path = File.expand_path(file)
+  return [] unless File.exist?(file_abs_path)
+  File.foreach(file_abs_path).map { |line|
+    next if line.strip.empty? || line.strip.start_with?('#')
+    line.split(separator, 2).map(&:strip)
+  }.to_h
 end
 
-def flutter_root(f)
-    while f != '/'
-        if File.exist?(File.join(f, 'flutter'))
-            return f
-        end
-        f = File.expand_path('..', f)
-    end
-    raise "Unable to find Flutter root"
+def flutter_root
+  File.expand_path(File.join('..', '..'), __dir__)
 end
 
-def flutter_install_all_ios_pods(ios_application_path = nil)
-    ios_application_path ||= File.dirname(File.realpath(__FILE__))
-    generated_plugins_path = File.join(ios_application_path, '.flutter-plugins-dependencies')
-    if !File.exist?(generated_plugins_path)
-        raise "#{generated_plugins_path} must exist. If you're running pod install manually, make sure \"flutter pub get\" is executed first"
-    end
+def flutter_ios_podfile_setup
+  ios_application_path = File.expand_path(File.join(__dir__, '..', '..'))
+  File.expand_path(ios_application_path)
+end
 
-    plugins = JSON.parse(File.read(generated_plugins_path))
-    plugins['plugins'].each do |plugin|
-        plugin_name = plugin['name']
-        plugin_path = plugin['path']
-        if plugin_path.nil?
-            raise "No path for plugin #{plugin_name}"
-        end
-        symlink = File.join(ios_application_path, plugin_name)
-        File.symlink(plugin_path, symlink) unless File.exist?(symlink)
-        pod plugin_name, :path => plugin_path
-    end
+def flutter_install_all_ios_pods(installer)
+  flutter_ios_podfile_setup
+  File.expand_path(File.join(installer, '..', 'Podfile'))
 end
