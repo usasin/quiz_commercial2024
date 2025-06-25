3e695b75bb1cd87c79ca8f251ac1c53c02d961b9
usasin <124710758+usasin@users.noreply.github.com>
2025-06-25 11:56:19 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 import 'screens/login_screen.dart';
 import 'screens/chapter_menu_page.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(MyApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp()); // ⬅️ NE PAS METTRE const ici !
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Quiz',
+      title: 'Quiz Commercial',
       debugShowCheckedModeBanner: false,
-      initialRoute: FirebaseAuth.instance.currentUser == null
-          ? '/login'
-          : '/chapter_menu',
+      initialRoute: '/',
       routes: {
-        '/login': (_) => const LoginScreen(),
+        '/': (_) => const LoginScreen(),
         '/chapter_menu': (_) => const ChapterMenuPage(),
       },
     );
