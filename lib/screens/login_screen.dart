010ce7f4197973ea8b7a631e10886be8c960f4f8
usasin <124710758+usasin@users.noreply.github.com>
2025-07-04 08:33:30 +0200

    Update login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,7 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'screens/chapter_menu_page.dart';
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
   @override
