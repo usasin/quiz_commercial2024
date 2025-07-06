75d1749bb76d970cf79e2cf6a0d922b216836a0d
usasin <124710758+usasin@users.noreply.github.com>
2025-07-06 09:14:58 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+
 import 'screens/login_screen.dart';
 import 'screens/chapter_menu_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'screens/levels_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(MyApp()); // PAS const ici
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Quiz Commercial',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Scaffold(
-              body: Center(child: CircularProgressIndicator()),
-            );
-          }
-          if (snapshot.hasData) {
-            return const ChapterMenuPage();
-          } else {
-            return const LoginScreen();
-          }
-        },
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (_) => LoginScreen(), // 🔁 ENLEVER const
+        '/chapter_menu': (_) => ChapterMenuPage(),
+        '/levels': (_) => LevelsPage(),
+      },
     );
   }
 }
